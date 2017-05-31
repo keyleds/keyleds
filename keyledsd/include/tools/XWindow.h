e0947a45bb5c42eaf3d4e799b949d1ddd29bedbc
Julien Hartmann <julien@etherdream.org>
2017-05-31 19:10:43 +0000

    Implement C++ wrappers around Xlib Window queries and Xinput2 devices. Scope limited to keyledsd.

---
+++ keyledsd/include/tools/XWindow.h
@@ -0,0 +1,122 @@
+#ifndef TOOLS_WINDOW_H
+#define TOOLS_WINDOW_H
+
+#include <X11/Xlib.h>
+#include <X11/extensions/XInput.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace xlib {
+
+class Display;
+
+/****************************************************************************/
+
+class Window final
+{
+public:
+    typedef ::Window handle_type;
+public:
+                            Window(Display & display, handle_type window)
+                             : m_display(display), m_window(window), m_classLoaded(false) {}
+
+    Display &               display() const { return m_display; }
+    handle_type             handle() const { return m_window; }
+
+    void                    changeAttributes(unsigned long mask, const XSetWindowAttributes & attrs);
+
+    std::string             name() const;
+    std::string             iconName() const;
+    const std::string &     className() const { if (!m_classLoaded) { loadClass(); } return m_className; }
+    const std::string &     instanceName() const { if (!m_classLoaded) { loadClass(); } return m_instanceName; }
+
+    std::string             getProperty(Atom atom, Atom type) const;
+
+private:
+    void                    loadClass() const;
+
+private:
+    Display &               m_display;
+    handle_type             m_window;
+    mutable std::string     m_className;
+    mutable std::string     m_instanceName;
+    mutable bool            m_classLoaded;
+};
+
+/****************************************************************************/
+
+class Device final
+{
+public:
+    typedef int             handle_type;
+    enum { RawKeyPress, RawKeyRelease, RawButtonPress, RawButtonRelease } event_type;
+public:
+                            Device(Display & display, handle_type device)
+                             : m_display(display), m_device(device) {}
+                            Device(const Device &) = delete;
+                            Device(Device && other) = default;
+
+    static Device open(Display &, const std::string & devNode);
+
+    Display &               display() const { return m_display; }
+    handle_type             handle() const { return m_device; }
+
+    std::string             getProperty(Atom atom, Atom type) const;
+
+private:
+    Display &               m_display;
+    handle_type             m_device;
+};
+
+/****************************************************************************/
+
+class Display final
+{
+public:
+    typedef std::unique_ptr<::Display, int(*)(::Display*)> display_ptr;
+    typedef display_ptr::pointer handle_type;
+    typedef std::unordered_map<std::string, Atom> atom_map;
+public:
+                            Display(std::string name = std::string());
+                            Display(const Display &) = delete;
+                            Display(Display &&) = default;
+    Display &               operator=(const Display &) = delete;
+    Display &               operator=(Display &&) = delete;
+                            ~Display();
+
+    const std::string &     name() const { return m_name; }
+    handle_type             handle() const { return m_display.get(); }
+    Window &                root() { return m_root; }
+    Atom                    atom(const std::string & name) const;
+
+    int                     connection() const;
+
+    std::unique_ptr<Window> getActiveWindow();
+
+private:
+    static display_ptr      openDisplay(const std::string &);
+
+private:
+    std::string             m_name;
+    display_ptr             m_display;
+    Window                  m_root;
+    mutable atom_map        m_atomCache;
+};
+
+/****************************************************************************/
+
+class Error : public std::runtime_error
+{
+public:
+                            Error(const std::string & msg)
+                             : std::runtime_error(msg) {}
+                            Error(::Display *display, XErrorEvent *event)
+                             : std::runtime_error(makeMessage(display, event)) {}
+private:
+    static std::string      makeMessage(::Display *display, XErrorEvent *event);
+};
+
+}
+
+#endif
---
+++ keyledsd/src/tools/XWindow.cxx
@@ -0,0 +1,195 @@
+#include <exception>
+#include <limits>
+#include <sstream>
+#include <X11/Xatom.h>
+#include <X11/extensions/XInput2.h>
+#include "tools/XWindow.h"
+
+const std::string activeWindowAtom = "_NET_ACTIVE_WINDOW";
+
+int errorHandler(Display * display, XErrorEvent * error)
+{
+    throw xlib::Error(display, error);
+}
+static auto initialHandler = XSetErrorHandler(errorHandler);
+
+/****************************************************************************/
+
+xlib::Display::Display(std::string name)
+    : m_name(name),
+      m_display(openDisplay(name)),
+      m_root(*this, XDefaultRootWindow(m_display.get()))
+{
+}
+
+xlib::Display::~Display()
+{
+}
+
+int xlib::Display::connection() const
+{
+    return XConnectionNumber(m_display.get());
+}
+
+Atom xlib::Display::atom(const std::string & name) const
+{
+    auto it = m_atomCache.find(name);
+    if (it != m_atomCache.cend()) {
+        return it->second;
+    }
+    Atom atom = XInternAtom(m_display.get(), name.c_str(), True);
+    m_atomCache[name] = atom;
+    return atom;
+}
+
+std::unique_ptr<xlib::Window> xlib::Display::getActiveWindow()
+{
+    std::string data = m_root.getProperty(atom(activeWindowAtom), XA_WINDOW);
+    ::Window handle = *reinterpret_cast<const ::Window *>(data.data());
+    if (handle == 0) { return std::unique_ptr<xlib::Window>(nullptr); }
+    return std::make_unique<xlib::Window>(*this, handle);
+}
+
+xlib::Display::display_ptr xlib::Display::openDisplay(const std::string & name)
+{
+    handle_type display = XOpenDisplay(name.empty() ? NULL : name.c_str());
+    if (display == nullptr) {
+        throw std::runtime_error("Failed to open display " + name);
+    }
+    return xlib::Display::display_ptr(display, XCloseDisplay);
+}
+
+/****************************************************************************/
+
+void xlib::Window::changeAttributes(unsigned long mask, const XSetWindowAttributes & attrs)
+{
+    XChangeWindowAttributes(m_display.handle(), m_window, mask, const_cast<XSetWindowAttributes*>(&attrs));
+}
+
+std::string xlib::Window::name() const
+{
+    std::string name = getProperty(
+        m_display.atom("_NET_WM_NAME"),
+        m_display.atom("UTF8_STRING")
+    );
+    if (name.empty()) {
+        name = getProperty(XA_WM_NAME, XA_STRING);
+    }
+    return name;
+}
+
+std::string xlib::Window::iconName() const
+{
+    return getProperty(XA_WM_ICON_NAME, XA_STRING);
+}
+
+std::string xlib::Window::getProperty(Atom atom, Atom type) const
+{
+    Atom actualType;
+    int actualFormat;
+    unsigned long nItems, bytesAfter;
+    unsigned char * value;
+    if (XGetWindowProperty(m_display.handle(), m_window, atom,
+                           0, std::numeric_limits<long>::max() / 4,
+                           False, type,
+                           &actualType, &actualFormat,
+                           &nItems, &bytesAfter, &value) != Success ||
+        actualType != type) {
+        return std::string();
+    }
+
+    // Wrap value in a unique_ptr in case something throws after that point
+    auto valptr = std::unique_ptr<unsigned char, int(*)(void*)>(value, XFree);
+
+    size_t itemBytes;
+    switch (actualFormat) {
+        case 8: itemBytes = sizeof(char); break;
+        case 16: itemBytes = sizeof(short); break;
+        case 32: itemBytes = sizeof(long); break;
+        default: throw std::logic_error("Invalid actualFormat returned by XGetWindowProperty");
+    }
+    return std::string(reinterpret_cast<char *>(valptr.get()), nItems * itemBytes);
+}
+
+void xlib::Window::loadClass() const
+{
+    m_className = getProperty(XA_WM_CLASS, XA_STRING);
+    auto sep = m_className.find('\0');
+    if (sep != std::string::npos) {
+        m_instanceName = m_className.substr(0, sep);
+        m_className.erase(0, sep + 1);
+        sep = m_className.find('\0');
+        if (sep != std::string::npos) {
+            m_className.erase(sep);
+        }
+    }
+    m_classLoaded = true;
+}
+
+/****************************************************************************/
+
+xlib::Device xlib::Device::open(Display & display, const std::string & devNode)
+{
+    int nbDevices;
+    auto devInfo = std::unique_ptr<XIDeviceInfo[], decltype(&XIFreeDeviceInfo)>(
+        XIQueryDevice(display.handle(), XIAllDevices, &nbDevices),
+        XIFreeDeviceInfo
+    );
+    if (devInfo == nullptr) { throw std::logic_error("XListInputDevices returned null"); }
+
+    Atom deviceNodeAtom = display.atom("Device Node");
+    for (int devIdx = 0; devIdx < nbDevices; ++devIdx) {
+        auto device = xlib::Device(display, devInfo[devIdx].deviceid);
+        auto node = device.getProperty(deviceNodeAtom, XA_STRING);
+        if (node == devNode) {
+            return device;
+        }
+    }
+    throw xlib::Error("Input device " + devNode + " not found");
+}
+
+std::string xlib::Device::getProperty(Atom atom, Atom type) const
+{
+    Atom actualType;
+    int actualFormat;
+    unsigned long nItems, bytesAfter;
+    unsigned char * value;
+    if (XIGetProperty(m_display.handle(), m_device, atom,
+                      0, std::numeric_limits<long>::max() / 4,
+                      False, type,
+                      &actualType, &actualFormat,
+                      &nItems, &bytesAfter, &value) != Success ||
+        actualType != type) {
+        return std::string();
+    }
+
+    // Wrap value in a unique_ptr in case something throws after that point
+    auto valptr = std::unique_ptr<unsigned char, int(*)(void*)>(value, XFree);
+
+    size_t itemBytes;
+    switch (actualFormat) {
+        case 8: itemBytes = sizeof(char); break;
+        case 16: itemBytes = sizeof(short); break;
+        case 32: itemBytes = sizeof(long); break;
+        default: throw std::logic_error("Invalid actualFormat returned by XGetDeviceProperty");
+    }
+    return std::string(reinterpret_cast<char *>(valptr.get()), nItems * itemBytes);
+}
+
+/****************************************************************************/
+
+std::string xlib::Error::makeMessage(::Display *display, XErrorEvent *event)
+{
+    std::ostringstream msg;
+
+    char buffer[256];
+    if (XGetErrorText(display, event->error_code, buffer, sizeof(buffer)) == Success) {
+        msg <<buffer;
+    } else {
+        msg <<"Error code " <<event->error_code;
+    }
+    msg <<" on display " <<DisplayString(display)
+        <<" while performing request " <<+event->request_code <<"." <<+event->minor_code;
+
+    return msg.str();
+}
