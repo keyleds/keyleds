e91da085041ef11b7e7b6739134ac8c41964220b
Julien Hartmann <julien@etherdream.org>
2019-02-21 14:49:01 +0000

    Update effects and configuration to use list values

--- keyledsd/effects/stars.lua
+++ keyledsd/effects/stars.lua
@@ -7,13 +7,23 @@ function stars(kb)
     local keys = keyleds.groups[keyleds.config.group] or keyleds.db
 
     local colors, transparents = {}, {}
-    for key, value in pairs(keyleds.config) do
-        if string.sub(key, 1, 5) == "color" then
-            local color = tocolor(value)
-            if color then
-                colors[#colors + 1] = color
-                transparents[#transparents + 1] = tocolor(color.red, color.red, color.blue, 0)
-            end
+
+    local colornames
+    if keyleds.config.colors then
+        colornames = keyleds.config.colors
+    else
+        colornames = {}
+        for key, value in pairs(keyleds.config) do
+            if string.sub(key, 1, 5) == "color" then colornames[#colornames + 1] = value end
+        end
+        print("numbered lists are deprecated, please check https://github.com/spectras/keyleds/wiki/Numbered-list-deprecation for help.")
+    end
+
+    for idx, value in ipairs(colornames) do
+        local color = tocolor(value)
+        if color then
+            colors[#colors + 1] = color
+            transparents[#transparents + 1] = tocolor(color.red, color.red, color.blue, 0)
         end
     end
     if #colors == 0 then colors[1], transparents[1] = tocolor('white'), tocolor(1, 1, 1, 0) end
